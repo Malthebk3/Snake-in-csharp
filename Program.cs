1a517113ba762068c583205f898784a783109f24
Malthe Kristensen <malthebk@Malthes-MacBook-Pro.local>
2025-02-13 09:29:32 +0100

    added dotnet

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+char coordinateGrid[11][11];
+
+int gridsize = 13, snakelength = 5, applePOSy, applePOSx;
+LinkedList<T>
+int[] 
+
+class main{
+    
+    
+    void createGrid(){
+        Console.SetCursorPosition(0, 0);
+
+        for (int x = 0; x <= mapSize; ++x){
+            for (int y = 0; y <= mapSize; ++y){
+                coordinateGrid[x][y]= '.';
+            }
+        }
+    }
+    
+    public static void main() {
+        
+    }
+}
