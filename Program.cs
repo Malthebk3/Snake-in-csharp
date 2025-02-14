4c62eb25cd99e9fe4f50cf004962b7d4eaf99561
Malthe Kristensen <malthebk@Malthes-MacBook-Pro.local>
2025-02-14 08:14:12 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -1,24 +1,74 @@
-﻿
-char coordinateGrid[11][11];
+﻿using System;
+using System.Collections.Generic;
 
-int gridsize = 13, snakelength = 5, applePOSy, applePOSx;
-LinkedList<T>
-int[] 
+public class Program{
+    
+    int gridsize = 17, snakelength = 5, applePOSy, applePOSx;
+    List<int> snakePOSx = new List<int>();
+    List<int> snakePOSy = new List<int>();
+    char[,] grid;
+    
 
-class main{
-    
-    
+    public Program() {
+        grid = new char[gridsize, gridsize];
+
+        int startX = gridsize / 2;
+        int startY = gridsize / 2;
+
+        for (int i = 0; i < snakelength; i++) {
+            snakePOSx.Insert(0, startX); 
+            snakePOSy.Insert(0, startY - i); 
+        }
+    }
+    static void Main(){     //MAIN PART RUN
+        int h = 0, v = -1;
+        Console.Clear();
+        Program mc = new Program();
+        mc.createGrid();
+
+        while(true) {
+            char input = Console.ReadKey().KeyChar;
+
+            if(input == 'a' && v != 1) { h = 0; v = -1; }
+            if(input == 'd' && v != -1) { h = 0; v = 1; }
+            if(input == 'w' && h != 1) { v = 0; h = -1; } 
+            if(input == 's' && h != -1) { v = 0; h = 1; }
+
+            mc.moveSnake(h, v);
+            mc.createGrid();
+        }
+    }
+
+    void moveSnake(int h, int v){
+        int currentPOSx = snakePOSx[0] + h;
+        int currentPOSy = snakePOSy[0] + v;
+
+
+        snakePOSx.Insert(0, currentPOSx);
+        snakePOSy.Insert(0, currentPOSy);
+
+        if (snakePOSx.Count > snakelength) { snakePOSx.RemoveAt(snakePOSx.Count - 1); }
+        if (snakePOSy.Count > snakelength) { snakePOSy.RemoveAt(snakePOSy.Count - 1); }
+    }
+
     void createGrid(){
         Console.SetCursorPosition(0, 0);
 
-        for (int x = 0; x <= mapSize; ++x){
-            for (int y = 0; y <= mapSize; ++y){
-                coordinateGrid[x][y]= '.';
+        for (int x = 0; x < gridsize; x++) {
+            for (int y = 0; y < gridsize; y++) {
+                grid[x, y] = '.';
             }
         }
-    }
-    
-    public static void main() {
-        
+
+        for (int i = 0; i < snakelength; ++i) {
+            grid[snakePOSx[i], snakePOSy[i]] = 'O';
+        }
+
+        for (int i = 0; i < gridsize; i++) {
+            for (int j = 0; j < gridsize; j++) {
+                Console.Write(grid[i, j] + "  ");
+            }
+            Console.WriteLine();
+        }
     }
 }
--- obj/Debug/net9.0/Snake-in-csharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/Snake-in-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake-in-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a06d92e543cca5383ddb0a4ab042d6221891d90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a517113ba762068c583205f898784a783109f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake-in-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake-in-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
